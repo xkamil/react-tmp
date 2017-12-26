678c679e7964555b914e258c1f0d7d9b6c046aa7
Kamil Wróbel <Kamil.Wrobel@pega.com>
2017-12-26 15:00:55 +0100

    Cleared project

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React, {Component} from 'react';
-import './App.css';
 import axios from 'axios'
-import UserInfo from './UserInfo'
-import Register from "./Register";
-
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            date: Date.now(),
-            time: 3,
-            users: []
-        };
-
-        setInterval(() => {
-            this.setState(Object.assign(this.state, {date: Date.now()}))
-        }, 10)
     }
 
     componentDidMount() {
-        axios.get("https://jsonplaceholder.typicode.com/users", {headers: {'apikey': localStorage.getItem('apikey')}})
+        axios.get("https://jsonplaceholder.typicode.com/users")
             .then(res => {
-                this.state.users = res.data;
-                if (!localStorage.getItem('apikey')) {
-                    localStorage.setItem('apikey', 'stupdkey')
-                }
+                console.log();
             })
     }
 
@@ -35,27 +18,9 @@ class App extends Component {
         return (
             <div className="container">
 
-                <Register/>
+                <h1>Aplikacja</h1>
             </div>
         );
-    }
-
-    tick() {
-        this.render();
-    }
-
-    list(elements) {
-        let list = [];
-
-        elements.forEach((element => {
-            list.push(<UserInfo user={element}/>)
-        }));
-
-        return (
-            <div className="ListOfUsers">
-                {list}
-            </div>
-        )
     }
 }
 
